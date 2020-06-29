0318a02a4fd8abb1d50f31dca31c6324bf201d34
Sean Miningah <miningahsean@gmail.com>
2020-06-30 00:57:25 +0300

    listings and realtors apps were created

--- btre/settings.py
+++ btre/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'realtors.apps.RealtorsConfig',
+    'listings.apps.ListingsConfig',
     'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -76,8 +78,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'btredb',
+        'USER':'postgres',
+        'PASSWORD': '0000',
+        'HOST': 'localhost'
     }
 }
 
