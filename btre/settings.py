0cdf08d140180bfdddcf6f6ad4b7fdaa2e570b22
Sean Miningah <miningahsean@gmail.com>
2020-07-17 20:25:17 +0300

    responsive dashboard and email functionality

--- btre/settings.py
+++ btre/settings.py
@@ -147,3 +147,10 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
     
 }
+
+#Email config
+EMAI_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAI_USE_TLS = True
